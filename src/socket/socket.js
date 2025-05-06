392e335a3fc37cdd15bb92ededeed945901bd321
mymoneyjunkie <mymoneyjunkie@gmail.com>
2025-05-06 19:31:41 +0530

    Update socket.js

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL; // or your deployed server IP/domain
 
-console.log(SOCKET_URL);
+//console.log(SOCKET_URL);
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"], // ensures real-time connection
